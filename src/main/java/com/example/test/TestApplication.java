7f8dca7e83d3ad753f7dcba458f9da1b3d90a5e2
rajni2711r <dell>
2025-10-01 09:04:49 +0530

    first commit

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+        System.out.println("Spring Boot Test Application Started...");
+    }
+
+    // Functions we want to test
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/example/test/TestApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestApplicationTest {
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void add() {
+    }
+}
